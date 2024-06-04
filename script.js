f8f5b597d9b2dad4f167fc525f9ef927885a9a27
JackB91 <you@example.com>
2024-06-03 22:36:38 -0700

    initial-design

---
+++ script.js
@@ -0,0 +1,31 @@
+// const images = document.querySelectorAll(".plus-img");
+// images.forEach(function (image) {
+//   image.addEventListener("click", function () {
+//     if (image.src.includes("icon-plus.svg")) {
+//       image.src = "/images/icon-minus.svg";
+//     } else {
+//       image.src = "/images/icon-plus.svg";
+//     }
+//   });
+// });
+
+const plus = document.getElementById("plus-img1");
+const answer1 = document.getElementById("answer1");
+
+plus.addEventListener("click", function () {
+  if (plus.src.includes("icon-plus.svg")) {
+    plus.src = "/images/icon-minus.svg";
+  } else {
+    plus.src = "/images/icon-plus.svg";
+  }
+
+  handleAnswer();
+});
+
+function handleAnswer() {
+  if (plus.src.includes("icon-minus.svg")) {
+    answer1.style.display = "block";
+  } else {
+    answer1.style.display = "none";
+  }
+}
